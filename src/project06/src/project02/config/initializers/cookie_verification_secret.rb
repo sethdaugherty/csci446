a82be1dc620a635cd9146abc9a1ee701a73b7938
Seth Daugherty <sdaugher@mines.edu>
2011-03-07 05:31:40 -0700

    Cleaning up repo

--- src/project06/src/project02/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'ecb17333969547cab9a1daf3d38637d9b4b5ebf30d6cac80da398940f4b991f5c21aebc52ead863c4119f0e0a68f7084b301b88a200a77c2f78927dc1675d3f0';
--- src/project06/src/project04/db/migrate/20110206002200_add_default_edit.rb
+++
@@ -1,8 +0,0 @@
-class AddDefaultEdit < ActiveRecord::Migration
-  def self.up
-    change_column :articles, :editedCount, :integer, :default => 0
-  end
-
-  def self.down
-  end
-end
